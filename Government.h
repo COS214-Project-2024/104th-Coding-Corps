8b6e09907f289d6e87457471f6b2c35e08f71f47
MarcoGeral <u23584565@tuks.co.za>
2024-10-30 22:33:55 +0200

    smart pointer fixes

--- Government.h
+++ Government.h
@@ -7,34 +7,34 @@
 #include "Budget.h"
 #include <string>
 #include <vector>
-
+#include <memory>
 
 class Government {
 
 private:
-	std::vector<AbstractCitizen*> citizenList; 
-	Command* command;
-	std::shared_ptr<ResourceManager> resourceManager;
-	static std::shared_ptr<Government> uniqueInstance;
-	std::shared_ptr<Budget> budget;
-	Government();
-	std::string currentTaxRatePolicy = "standard";
+    std::vector<std::shared_ptr<AbstractCitizen>> citizenList; // Use smart pointers for citizens
+    std::shared_ptr<Command> command;                           // Use a smart pointer for the command
+    std::shared_ptr<ResourceManager> resourceManager;
+    static std::shared_ptr<Government> uniqueInstance;
+    std::shared_ptr<Budget> budget;
+    Government();
+    std::string currentTaxRatePolicy = "standard";
 
 public:
     Government(const Government&) = delete;
     void operator=(const Government&) = delete;
-	static std::shared_ptr<Government> getInstance();
-	void setCommand(Command* c);
-	void issueCommand();
-	void notifyResourceChange(std::string resourceType, int quantity);
-	void addResourceToCity(const std::string& resourceType, int quantity, ResourceFactory& factory);
-	bool useResource(const std::string& type, int quantity);
-	void displayCityResources();
-	void increaseBudget(double amount);
-	void decreaseBudget(double amount);
-	double getBalance() const;
-	void setTaxRatePolicy(const std::string& taxRatePolicy);
-	std::string getTaxRatePolicy() const;
+    static std::shared_ptr<Government> getInstance();
+    void setCommand(const std::shared_ptr<Command>& c);        // Accept shared pointer for command
+    void issueCommand();
+    void notifyResourceChange(const std::string& resourceType, int quantity);
+    void addResourceToCity(const std::string& resourceType, int quantity, ResourceFactory& factory);
+    bool useResource(const std::string& type, int quantity);
+    void displayCityResources();
+    void increaseBudget(double amount);
+    void decreaseBudget(double amount);
+    double getBalance() const;
+    void setTaxRatePolicy(const std::string& taxRatePolicy);
+    std::string getTaxRatePolicy() const;
 };
 
 #endif
