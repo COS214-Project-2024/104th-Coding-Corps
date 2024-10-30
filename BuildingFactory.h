ed01803059d59e96c999e4099214c2e6940ef133
Capleton11 <u22554875@tuks.co.za>
2024-10-30 23:53:13 +0200

    fgh

--- BuildingFactory.h
+++ BuildingFactory.h
@@ -7,6 +7,7 @@
 
 class BuildingFactory {
 public:
+<<<<<<< HEAD
     virtual std::unique_ptr<BuildingComponent> createFlat(int units, int residents, int floors, bool hasGarden) { return nullptr; }
     virtual std::unique_ptr<BuildingComponent> createHouse(int garageSize, bool hasPool, int residents, int floors, bool hasGarden) { return nullptr; }
     virtual std::unique_ptr<BuildingComponent> createEstate(int garageSize, bool hasPool, int residents, int floors, bool hasGarden, int units) { return nullptr; }
@@ -19,6 +20,20 @@ public:
     virtual std::unique_ptr<BuildingComponent> createSchool(int highestQualification, int quality, int numWorkers) { return nullptr; }
     virtual std::unique_ptr<BuildingComponent> createUniversity(int highestQualification, int quality, int numWorkers) { return nullptr; }
     virtual std::unique_ptr<BuildingComponent> createHospital(int quality, int numWorkers) { return nullptr; }
+=======
+	virtual BuildingComponent* createFlat(int units, int residents, int floors, bool hasGarden) { return nullptr; }
+	virtual BuildingComponent* createHouse(int garageSize, bool hasPool, int residents, int floors, bool hasGarden) {return nullptr;}
+	virtual BuildingComponent* createEstate(int garageSize, bool hasPool, int residents, int floors, bool hasGarden, int units) {return nullptr;}
+	virtual BuildingComponent* createFactory(int numWorkers, string factoryType, double production) {return nullptr;}
+	virtual BuildingComponent* createPlant(int workers, double production) {return nullptr;}
+	virtual BuildingComponent* createWarehouse(int storageCapacity, int numWorkers) {return nullptr;}
+	virtual shared_ptr<BuildingComponent> createOffice(int numEmployees, string businessType, int x, int y, const std::string& district, int quality);
+    virtual shared_ptr<BuildingComponent> createMall(int numShops, string businessType, int numBusinesses, int x, int y, const std::string& district, int quality);
+    virtual shared_ptr<BuildingComponent> createShop(int shopSize, string businessType, int x, int y, const std::string& district, int quality);
+	virtual BuildingComponent* createSchool(int highestQualification, int quality, int numWorkers) {return nullptr;}
+	virtual BuildingComponent* createUniversity(int highestQualification, int quality, int numWorkers) {return nullptr;}
+	virtual BuildingComponent* createHospital(int quality, int numWorkers) {return nullptr;}
+>>>>>>> 7994e6e602aaa960bf2d086fd911a33ff53c6845
 };
 
 #endif
