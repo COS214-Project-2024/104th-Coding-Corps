83b99076581ec4c79cd4f3cb8a559334d4592556
SaberF24 <sabfa24@gmail.com>
2024-11-04 11:11:31 +0200

    Added debugging

--- sourceCode/SavePointManager.cpp
+++ sourceCode/SavePointManager.cpp
@@ -24,6 +24,10 @@ void SavePointManager::saveState(std::shared_ptr<SavePoint> savePoint) {
 
     savePoints.push_back(savePoint);
     currentSaveIndex = savePoints.size() - 1;
+
+    // Debugging information
+    std::cout << "[DEBUG] SavePoint added. Total SavePoints: " << savePoints.size() 
+              << ", Current Index: " << currentSaveIndex << "\n";
 }
 
 /**
@@ -38,8 +42,10 @@ void SavePointManager::saveState(std::shared_ptr<SavePoint> savePoint) {
 std::shared_ptr<SavePoint> SavePointManager::undo() {
     if (currentSaveIndex > 0) {
         currentSaveIndex--;
+        std::cout << "[DEBUG] Undoing to SavePoint. Current Index: " << currentSaveIndex << "\n";
         return savePoints[currentSaveIndex];
     }
+    std::cout << "[DEBUG] No previous SavePoint to return to.\n";
     return nullptr;  
 }
 
--- sourceCode/SavePointManager.h
+++ sourceCode/SavePointManager.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <memory>
+#include <iostream>
 #include "SavePoint.h"
 
 /**
